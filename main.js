1767bf4d737bb41d2cdd56d265ddf0b99cbb1748
gulapatla <76089049+gulapatla@users.noreply.github.com>
2021-09-11 14:47:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+img = "";
+status = "";
+objects = [];
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : detecting objects";
+
+
+
+}
+function preload(){
+    img = loadImage('https://i.postimg.cc/SQ2MscW7/dog-cat.jpg');
+}
+function draw()
+{
+    image(img, 0,0,640,420);
+    if(status != ""){
+        for (i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "status : object detected";
+
+
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%" , objects[i].x , objects[i].y);
+            noFill("");
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+
+
+        }
+
+    }
+
+    
+
+
+    fill("#FF0000");
+    text("dog", 45,75);
+    noFill();
+    stroke("#FF0000");
+    rect(30,60,400,350);
+
+
+    fill("#FF0000");
+    text("cat", 320, 120);
+    noFill();
+    stroke("FF0000"); 
+    rect(300, 90, 270, 320);   
+
+}
+function modelLoaded(){
+    console.log("modelLoaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+
+    
+    
+
+}
